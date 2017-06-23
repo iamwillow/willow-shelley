75102eba4c820454af49e2f012d09c723ce44e40
Willow Shelley <willow.shelley@rakuten.com>
2017-06-23 10:31:26 +0900

    removed old files, added react boilerplate and examples

--- school/money.php
+++
@@ -1,9 +0,0 @@
-<?
-// Penny on the first day. 
-// Money is doubled every day thereafter
-$money = .01;
-for($i = 1; $i<32; $i++){
-	echo 'Day:' . $i . ': $' . $money . '<br/>';
-	$money *= 2;
-}
-?>
