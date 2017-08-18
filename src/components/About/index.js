c6b252276bf7d943921b41de2fb56550538916db
Willow Shelley <willow.shelley@rakuten.com>
2017-08-18 16:52:48 -0400

    about page basic setup, beginning of bubbleFloat keyframing

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,25 @@ class About extends React.Component {
     return (
       <div className='about'>
         <div className='max-width'>
-          <h1>test</h1>
+        	<div className="col-sm-8 dino-tube-wrapper">
+        		<img className="bubble bubble-1" src="../src/assets/img/bubble-1.png"/>
+	          <img className="dino-tube dino-1" src="../src/assets/img/dino-1.png"/>
+
+	          <img className="bubble bubble-2" src="../src/assets/img/bubble-2.png"/>
+	          <img className="dino-tube dino-2" src="../src/assets/img/dino-2.png"/>
+
+	          <img className="bubble bubble-3" src="../src/assets/img/bubble-3.png"/>
+	          <img className="dino-tube dino-3" src="../src/assets/img/dino-3.png"/>
+
+	          <img className="bubble bubble-4" src="../src/assets/img/bubble-4.png"/>
+	          <img className="dino-tube dino-4" src="../src/assets/img/dino-4.png"/>
+
+	          <img className="bubble bubble-5" src="../src/assets/img/bubble-5.png"/>
+	          <img className="dino-tube dino-5" src="../src/assets/img/dino-5.png"/>
+	         </div>
+	         <div className="col-sm-4">
+        		<h2>Let my skillsets be the <b>DNA</b> of your creation.</h2>
+        	</div>
         </div>
       </div>
     );
